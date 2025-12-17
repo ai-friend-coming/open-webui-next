0635544d4049c33041d2316e00591ad19958d224
Gaofeng <u3011873@connect.hku.hk>
2025-12-17 11:51:59 +0800

    调整添加私有模型按钮的位置

--- src/lib/components/chat/ModelSelector/Selector.svelte
+++ src/lib/components/chat/ModelSelector/Selector.svelte
@@ -14,16 +14,16 @@
 
 	import { deleteModel, getOllamaVersion, pullModel, unloadModel } from '$lib/apis/ollama';
 
-import {
-	user,
-	MODEL_DOWNLOAD_POOL,
-	models,
-	mobile,
-	temporaryChatEnabled,
-	settings,
-	config,
-	modelPricings
-} from '$lib/stores';
+	import {
+		user,
+		MODEL_DOWNLOAD_POOL,
+		models,
+		mobile,
+		temporaryChatEnabled,
+		settings,
+		config,
+		modelPricings
+	} from '$lib/stores';
 	import { toast } from 'svelte-sonner';
 	import { capitalizeFirstLetter, sanitizeResponseContent, splitStream } from '$lib/utils';
 	import { getModels } from '$lib/apis';
@@ -33,9 +33,9 @@ import {
 	import Search from '$lib/components/icons/Search.svelte';
 	import Tooltip from '$lib/components/common/Tooltip.svelte';
 	import Switch from '$lib/components/common/Switch.svelte';
-import ChatBubbleOval from '$lib/components/icons/ChatBubbleOval.svelte';
+	import ChatBubbleOval from '$lib/components/icons/ChatBubbleOval.svelte';
 
-import ModelItem from './ModelItem.svelte';
+	import ModelItem from './ModelItem.svelte';
 
 	const i18n = getContext('i18n');
 	const dispatch = createEventDispatcher();
@@ -460,6 +460,35 @@ import ModelItem from './ModelItem.svelte';
 				</div>
 			{/if}
 
+			<!-- Add private model button -->
+			<div class="mx-4">
+				<button
+					class="w-full text-left px-2 py-1.5 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-800/60 flex items-center gap-2 transition mb-1"
+					on:click={() => {
+						addUserModel();
+						show = false;
+					}}
+				>
+					<div
+						class="size-7 rounded-full bg-gray-900 text-white dark:bg-gray-100 dark:text-gray-900 flex items-center justify-center"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 16 16"
+							fill="currentColor"
+							class="size-4"
+						>
+							<path
+								d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z"
+							/>
+						</svg>
+					</div>
+					<div class="text-sm font-medium text-gray-900 dark:text-gray-100">
+						{$i18n.t('Add My API')}
+					</div>
+				</button>
+			</div>
+
 			<div class="px-2">
 				{#if tags && items.filter((item) => !(item.model?.info?.meta?.hidden ?? false)).length > 0}
 					<div
@@ -539,33 +568,6 @@ import ModelItem from './ModelItem.svelte';
 			</div>
 
 			<div class="px-2.5 max-h-128 overflow-y-auto group relative">
-				<!-- Add private model button -->
-				<button
-					class="w-full text-left px-2 py-1.5 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-800/60 flex items-center gap-2 transition mb-1"
-					on:click={() => {
-						addUserModel();
-						show = false;
-					}}
-				>
-					<div
-						class="size-7 rounded-full bg-gray-900 text-white dark:bg-gray-100 dark:text-gray-900 flex items-center justify-center"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 16 16"
-							fill="currentColor"
-							class="size-4"
-						>
-							<path
-								d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z"
-							/>
-						</svg>
-					</div>
-					<div class="text-sm font-medium text-gray-900 dark:text-gray-100">
-						{$i18n.t('Add My API')}
-					</div>
-				</button>
-
 				{#each filteredItems as item, index}
 					<ModelItem
 						{selectedModelIdx}
